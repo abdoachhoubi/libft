1fe41be3b7562e12abaaedb4073bac0db2c88592
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-20 22:34:10 +0100

    Update recently added prototypes

--- libft.h
+++ libft.h
@@ -4,6 +4,8 @@
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 void	ft_putnbr(int nb);
-int	ft_strlen(char *str);
+int	ft_strlen(const char *str);
 char	*ft_strchr(char *str, char c);
+char	*ft_strcpy(char *s1, const char s2);
+char	*ft_strdup(const char *src);
 #endif
