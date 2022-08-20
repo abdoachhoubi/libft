b4e04b1f5043c32322647318e6e6163ae9d5e0f4
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-20 21:59:50 +0100

    Added ft_putnbr

---
+++ ft_putnbr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		ft_putstr("-2147483648");
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
