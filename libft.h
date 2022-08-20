ae4f97d40f4438dc1f2647d7273faab39a1e2725
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-20 22:01:02 +0100

    Added libft header

---
+++ libft.h
@@ -0,0 +1,9 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+int	ft_strlen(char *str);
+char	*ft_strchr(char *str, char c);
+#endif
