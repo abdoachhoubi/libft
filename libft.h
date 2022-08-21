7c5fa1ad6a335d8c3d12db7f72c8216932396e60
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-21 21:48:42 +0100

    Update recently added functions

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@ char	*ft_strchr(char *s, int c);
 size_t	*ft_strlcpy(char *s1, const char *s2, size_t len);
 char	*ft_strdup(const char *s1);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
