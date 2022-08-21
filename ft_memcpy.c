0183638e8137a71fc96ea09bf7f8af58374e9476
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-21 21:48:02 +0100

    Added ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	unsigned char	*s1;
+	const unsigned char	*s2;
+
+	i = 0;
+	s1 = (unsigned char *)dest;
+	s2 = (const unsigned char *)src;
+	while (i < n)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return (dest);
+}
