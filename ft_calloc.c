c3db357036aec89ae9ba54a03cfb5cebe8a48eb7
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-22 16:34:24 +0100

    Added ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*mem;
+
+	mem = malloc(count * size);
+	if (!mem)
+		return (0);
+	ft_bzero(mem, count * size);
+	return (mem);
+}
