c61505c1b45cd5a8e3790ca9f6513ebb847b7fc4
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-22 16:34:40 +0100

    Added ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	total_len;
+	char	*str;
+
+	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(total_len * sizeof(char));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s1, total_len);
+	ft_strlcat(str, s2, total_len);
+	str[total_len - 1] = '\0';
+	return (str);
+}
