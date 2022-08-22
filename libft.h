4afd637d247ce08c481c8072604dc8f1d09a0b2a
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-22 13:16:46 +0100

    Update recently added functions

--- libft.h
+++ libft.h
@@ -18,4 +18,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_x(void *dest, const void *src, size_t n);
+int	ft_isascii(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 #endif
