d4f3e02e84756a36f6305f3acc76418be0a552e1
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-23 13:43:14 +0100

    Added ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aachhoub <aachhoub@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 13:18:52 by aachhoub          #+#    #+#             */
+/*   Updated: 2022/08/23 13:25:14 by aachhoub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*m;
+
+	m = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (m[i] == c)
+			return ((void *)(s + i));
+		i++;
+	}
+	return (0);
+}
