c335ccf0ceda751ff35ac32b80a6f4470a4e8264
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-25 21:17:42 +0100

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: aachhoub <aachhoub@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:33:48 by aachhoub          #+#    #+#             */
-/*   Updated: 2022/08/23 12:17:34 by aachhoub         ###   ########.fr       */
+/*   Updated: 2022/08/25 21:15:10 by aachhoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	size_t	j;
 
 	i = 0;
-	if (!needle)
+	if (*needle == '\0')
 		return ((char *)haystack);
+	j = ft_strlen(needle);
 	while (haystack[i] != '\0' && i < n)
 	{
-		j = 0;
-		while (needle[j] != '\0' && needle[j] == haystack[i + j])
-			j++;
-		if (needle[j] == '\0')
-			return ((char *)(haystack + i));
+		if (haystack[i] == needle[0] && n - i >= j
+			&& ft_strncmp(&haystack[i], needle, j) == 0)
+			return ((char *)&haystack[i]);
 		i++;
 	}
 	return (0);
