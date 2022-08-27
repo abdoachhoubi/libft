13d5a3580bb57fbd968bf9cb5d9825fd5d8aabfc
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-08-27 02:21:44 +0100

    Added ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: aachhoub <aachhoub@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 01:01:05 by aachhoub          #+#    #+#             */
-/*   Updated: 2022/08/25 01:09:55 by aachhoub         ###   ########.fr       */
+/*   Updated: 2022/08/27 02:08:54 by aachhoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (lst && new)
 	{
 		temp = *lst;
-		if (temp != NULL)
+		if (*lst)
 		{
 			while (temp -> next)
 				temp = temp -> next;
 			temp -> next = new;
 		}
 		else
-			temp = new;
+			*lst = new;
 	}
 }
